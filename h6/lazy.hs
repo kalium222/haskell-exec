a670adf5ffb709e2fbf6cba0e39a9205c479e029
kalium <kalium222@outlook.com>
2025-07-01 17:13:11 +0800

    h6

--- h6/lazy.hs
+++ h6/lazy.hs
@@ -22,3 +22,34 @@ fibs1 = [fib x | x <- [0 ..]]
 -- NOTE: 离谱
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
+
+-- Ex. 3
+-- NOTE: 😑😓🙄😅😰
+data Stream a = Stream a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Stream x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show = unwords . take 5 . map show . streamToList
+
+-- Ex. 4
+streamRepeat :: a -> Stream a
+streamRepeat a = Stream a (streamRepeat a)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Stream x xs) = Stream (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a0 = Stream a0 $ streamFromSeed f $ f a0
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+ruler :: Stream Integer
+ruler = streamMap h (streamFromSeed (+ 1) 1)
+
+h :: Integer -> Integer
+h x = y
+  where
+    y = maximum [a | a <- [0 .. x], mod x (2 ^ a) == 0]
