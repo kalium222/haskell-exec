9a638582b35de86af3a678457910e9dc83c195b6
kalium <kalium222@outlook.com>
2026-02-12 00:10:43 -0500

    ex1

--- h10/AParser.hs
+++ h10/AParser.hs
@@ -1,22 +1,10 @@
-{- CIS 194 HW 10
-   due Monday, 1 April
--}
-
 module AParser where
 
 import Control.Applicative
 import Data.Char
 
--- A parser for a value of type a is a function which takes a String
--- represnting the input to be parsed, and succeeds or fails; if it
--- succeeds, it returns the parsed value along with the remainder of
--- the input.
 newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
 
--- For example, 'satisfy' takes a predicate on Char, and constructs a
--- parser which succeeds only if it sees a Char that satisfies the
--- predicate (which it then returns).  If it encounters a Char that
--- does not satisfy the predicate (or an empty input), it fails.
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
@@ -27,24 +15,9 @@ satisfy p = Parser f
       | p x = Just (x, xs)
       | otherwise = Nothing -- otherwise, fail
 
--- Using satisfy, we can define the parser 'char c' which expects to
--- see exactly the character c, and fails otherwise.
 char :: Char -> Parser Char
 char c = satisfy (== c)
 
-{- For example:
-
-\*Parser> runParser (satisfy isUpper) "ABC"
-Just ('A',"BC")
-\*Parser> runParser (satisfy isUpper) "abc"
-Nothing
-\*Parser> runParser (char 'x') "xyz"
-Just ('x',"yz")
-
--}
-
--- For convenience, we've also provided a parser for positive
--- integers.
 posInt :: Parser Integer
 posInt = Parser f
   where
@@ -54,6 +27,16 @@ posInt = Parser f
       where
         (ns, rest) = span isDigit xs
 
-------------------------------------------------------------
--- Your code goes below here
-------------------------------------------------------------
+-- Ex.1
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (a, c) = (f a, c)
+
+instance Functor Parser where
+  fmap a2b (Parser runParser_a) = Parser runParser_b
+    where
+      runParser_b = fmap (first a2b) . runParser_a
+
+-- Ex.2
+instance Applicative Parser where
+  pure = undefined
+  (<*>) = undefined
